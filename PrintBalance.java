936a1167670c977d815c42a410a68c3bd48e9e77
Ismail Allana <ismailallana@gmail.com>
2021-06-08 03:02:41 +1000

    Add files via upload

---
+++ PrintBalance.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.text.NumberFormat;
+
+public class PrintBalance {
+    private MortgageCalculator calculator;
+
+    public PrintBalance(MortgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (double balance: calculator.getRemainingBalance())
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+}
